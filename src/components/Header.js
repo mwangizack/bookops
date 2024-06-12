b77e04bd0f6e08c75503540902ff2d4ca4d15aae
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-06-12 22:58:01 +0300

    Style the Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import PowerSettingsNewOutlinedIcon from '@mui/icons-material/PowerSettingsNewOutlined';
 
 function Header() {
   return (
     <header>
       <Link to='/' className='logo'>BookOps</Link>
-      <Link to='/login'>Logout</Link>
+      <div className='logout-section'>
+        <Link to='/login' className='logout'>Logout</Link>
+        <PowerSettingsNewOutlinedIcon fontSize='small' />
+      </div>
     </header>
   )
 }
