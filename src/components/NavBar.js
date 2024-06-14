9f582bbc26ea8fbe384a70764f7e00926fe5a6ed
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-06-14 12:22:05 +0300

    feat: Style the NavBar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import UserDetails from './UserDetails'
 import { NavLink } from 'react-router-dom'
+import SpeedIcon from '@mui/icons-material/Speed';
+import LibraryBooksOutlinedIcon from '@mui/icons-material/LibraryBooksOutlined';
 
 function NavBar() {
   return (
     <nav>
       <UserDetails />
+
       <p className='nav-group-title'>CORE</p>
-      <NavLink className='nav-link' to='/'>Dashboard</NavLink>
+
+      <div className='nav-item'>
+        <SpeedIcon fontSize='small'/>
+        <NavLink className='nav-link' to='/'>Dashboard</NavLink>
+      </div>
+
       <p className='nav-group-title'>BOOKS MANAGEMENT</p>
-      <NavLink className='nav-link' to='/books'>Books</NavLink>
+      
+      <div className='nav-item'>
+        <LibraryBooksOutlinedIcon fontSize='small' />
+        <NavLink className='nav-link' to='/books'>Books</NavLink>
+      </div>
     </nav>
   )
 }
--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import Stack from '@mui/material/Stack';
 
 function UserDetails() {
   return (
-    <div className='user-details'>
-      <i className='user-icon'></i>
-      <div>
+    <Stack direction="row" spacing={1} className='user-details'>
+      <AccountCircleIcon sx={{ fontSize: 50, color: '#2f4f4f' }} />
+      <div className='user-name-role'>
         <p className='user-role'>Administrator</p>
         <p className='username'>Username</p>
       </div>
-    </div>
+    </Stack>
   )
 }
 
