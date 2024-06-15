63c1c85adb2e55dc32a826fe6f97329a976f5864
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-06-16 01:55:27 +0300

    fix: Add a spinner as dashboard data loads

--- src/components/DashboardCard.js
+++ src/components/DashboardCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import CircularProgress from '@mui/material/CircularProgress';
 
 function DashboardCard({ cardData }) {
   const { cardTitle, count, route } = cardData;
@@ -7,7 +8,7 @@ function DashboardCard({ cardData }) {
   return (
     <div className="dashboard-card">
       <p className="dashboard-card-title">{cardTitle}</p>
-      <p className="dashboard-card-metric">{!count ? 'Loading...': count}</p>
+      <p className="dashboard-card-metric">{!count ? <CircularProgress />: count}</p>
       <Link to={route} className="dashboard-card-link">View Details</Link>
     </div>
   );
