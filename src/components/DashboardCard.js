0088174dd37091b6116c0199ee424d55635b976f
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-06-16 14:58:21 +0300

    fix: Change the size of the dashboard spinners

--- src/components/DashboardCard.js
+++ src/components/DashboardCard.js
@@ -8,7 +8,7 @@ function DashboardCard({ cardData }) {
   return (
     <div className="dashboard-card">
       <p className="dashboard-card-title">{cardTitle}</p>
-      <p className="dashboard-card-metric">{!count ? <CircularProgress />: count}</p>
+      <p className="dashboard-card-metric">{!count ? <CircularProgress size='small'/> : count}</p>
       <Link to={route} className="dashboard-card-link">View Details</Link>
     </div>
   );
