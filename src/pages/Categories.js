1ee97352a26879cba147d21756b57dbb3fab2646
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-06-17 00:14:37 +0300

    fix: Remove unused variables and imports

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,10 +1,7 @@
 import React from "react";
 import PageTitle from "../components/PageTitle";
-import { useOutletContext } from "react-router-dom";
 
 function Categories() {
-  const globalData = useOutletContext();
-  const books = globalData.books;
 
   return (
     <div className="categories-section">
