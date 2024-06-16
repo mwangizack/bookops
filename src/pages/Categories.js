bc5d34f25bf11ab7a2016da1a3823bfe95d2fe4a
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-06-16 23:07:13 +0300

    Style categories page

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,14 +1,17 @@
-import React from 'react'
-import PageTitle from '../components/PageTitle'
+import React from "react";
+import PageTitle from "../components/PageTitle";
+import { useOutletContext } from "react-router-dom";
 
 function Categories() {
+  const globalData = useOutletContext();
+  const books = globalData.books;
+
   return (
-    <div className='categories-section'>
-      <PageTitle title='Categories'/>
+    <div className="categories-section">
+      <PageTitle title="Categories" />
       <section className="categories-content-section"></section>
     </div>
-    
-  )
+  );
 }
 
-export default Categories
+export default Categories;
